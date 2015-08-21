0c6f258c7f81509117166eb990a618628cb165c0
virmerson <virmerson@hightechcursos.com.br>
2015-08-21 18:31:31 -0400

    Commit test

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Virmerson Bento dos Santos on 21/08/15.
+ */
+public class Calculadora {
+
+    /**
+     * Somando vários inteiros
+     * @param n
+     * @return
+     */
+    public int calcularSoma(int ... n){
+        int soma = 0;
+        for (int i: n){
+            soma = soma+i;
+        }
+        return soma;
+    }
+}
---
+++ src/main/test/TestCalculadora.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Virmerson Bento dos Santos on 21/08/15.
+ */
+public class TestCalculadora {
+
+
+    public void testSomar (){
+
+
+    }
+}
