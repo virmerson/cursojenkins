3e5fa3a896f321f8e75ec54b5508c12473e75c61
virmerson <virmerson@hightechcursos.com.br>
2015-08-21 22:28:34 -0400

    Commit test com erro

--- src/main/test/TestCalculadora.java
+++ src/main/test/TestCalculadora.java
@@ -1,11 +1,16 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
 /**
  * Created by Virmerson Bento dos Santos on 21/08/15.
  */
 public class TestCalculadora {
 
-
+    @Test
     public void testSomar (){
-
+        Calculadora c = new Calculadora();
+        int soma = c.calcularSoma(5, 5, 5);
+        Assert.assertEquals(soma, 16);
 
     }
 }
